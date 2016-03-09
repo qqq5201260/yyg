66f043421d4e722f47e5b52272406b048ccf59f1
czl <czl@czl.local>
2016-03-09 08:55:02 +0800

    完成推送

--- yyg/AppDelegate.m
+++ yyg/AppDelegate.m
@@ -20,6 +20,8 @@
 #import "AppDelegate.h"
 #import <BmobSDK/Bmob.h>
 #import "ZLFMDBHelp.h"
+#define UMSYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#define _IPHONE80_ 80000
 @interface AppDelegate ()
 
 @end
@@ -30,12 +32,96 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // 向bmob服务器注册你自己的应用
     [Bmob registerWithAppKey:@"1891b2a0dc45764948491db62a3e9798"];
-    [ZLFMDBHelp FMDBHelp];
+//    [ZLFMDBHelp FMDBHelp];
     // Override point for customization after application launch.
+   
+        //set AppKey and LaunchOptions
+        [UMessage startWithAppkey:@"56decc7267e58eb06400228d" launchOptions:launchOptions];
+    
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= _IPHONE80_
+        if(UMSYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"8.0"))
+        {
+            //register remoteNotification types
+            UIMutableUserNotificationAction *action1 = [[UIMutableUserNotificationAction alloc] init];
+            action1.identifier = @"action1_identifier";
+            action1.title=@"Accept";
+            action1.activationMode = UIUserNotificationActivationModeForeground;//当点击的时候启动程序
+            
+            UIMutableUserNotificationAction *action2 = [[UIMutableUserNotificationAction alloc] init];  //第二按钮
+            action2.identifier = @"action2_identifier";
+            action2.title=@"Reject";
+            action2.activationMode = UIUserNotificationActivationModeBackground;//当点击的时候不启动程序，在后台处理
+            action2.authenticationRequired = YES;//需要解锁才能处理，如果action.activationMode = UIUserNotificationActivationModeForeground;则这个属性被忽略；
+            action2.destructive = YES;
+            
+            UIMutableUserNotificationCategory *categorys = [[UIMutableUserNotificationCategory alloc] init];
+            categorys.identifier = @"category1";//这组动作的唯一标示
+            [categorys setActions:@[action1,action2] forContext:(UIUserNotificationActionContextDefault)];
+            
+            UIUserNotificationSettings *userSettings = [UIUserNotificationSettings settingsForTypes:UIUserNotificationTypeBadge|UIUserNotificationTypeSound|UIUserNotificationTypeAlert
+                                                                                         categories:[NSSet setWithObject:categorys]];
+            [UMessage registerRemoteNotificationAndUserNotificationSettings:userSettings];
+            
+        } else{
+            //register remoteNotification types
+            [UMessage registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge
+             |UIRemoteNotificationTypeSound
+             |UIRemoteNotificationTypeAlert];
+        }
+#else
+        
+        //register remoteNotification types
+        [UMessage registerForRemoteNotificationTypes:UIRemoteNotificationTypeBadge
+         |UIRemoteNotificationTypeSound
+         |UIRemoteNotificationTypeAlert];
+        
+#endif
+
+        //for log
+        [UMessage setLogEnabled:YES];
     
     return YES;
 }
 
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    NSLog(@"deviceToken:%@",[[[[deviceToken description] stringByReplacingOccurrencesOfString: @"<" withString: @""]
+                  stringByReplacingOccurrencesOfString: @">" withString: @""]
+                 stringByReplacingOccurrencesOfString: @" " withString: @""]);
+    [UMessage registerDeviceToken:deviceToken];
+    
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    //如果注册不成功，打印错误信息，可以在网上找到对应的解决方案
+    NSString *error_str = [NSString stringWithFormat: @"%@", error];
+    NSLog(@"Failed to get token, error:%@", error_str);
+    //如果注册成功，可以删掉这个方法
+    NSLog(@"application:didFailToRegisterForRemoteNotificationsWithError: %@", error);
+}
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    //关闭友盟自带的弹出框
+      [UMessage setAutoAlert:YES];
+    
+    [UMessage didReceiveRemoteNotification:userInfo];
+    
+        self.userInfo = userInfo;
+        //定制自定的的弹出框
+        if([UIApplication sharedApplication].applicationState == UIApplicationStateActive)
+        {
+            UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"标题"
+                                                                message:@"Test On ApplicationStateActive"
+                                                               delegate:self
+                                                      cancelButtonTitle:@"确定"
+                                                      otherButtonTitles:nil];
+    
+            [alertView show];
+            
+        }
+}
 
 
 - (void)applicationWillResignActive:(UIApplication *)application {
